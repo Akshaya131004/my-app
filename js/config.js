03e4a21343ecbe4de42e780f0751d360389a123e
Sugeerthi02 <sugeerthiiii@gmail.com>
2025-11-12 15:23:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_Dm338c296', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '33a9f0ko94pomgp2vutatqnkrp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_17pCMicjf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4tplujeht4gf5s3gm2bdj3e58d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://bl8zhww7q5.execute-api.ap-south-1.amazonaws.com/wildrydes' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
