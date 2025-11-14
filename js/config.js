e8be30d3422fc05020c5cacc4bc7a2b94e3b53fa
Akshaya131004 <akshayad617@gmail.com>
2025-11-14 11:38:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_17pCMicjf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4tplujeht4gf5s3gm2bdj3e58d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_yeLW8Y0Sq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '25veg6i4mg0fldp3nhndchug9l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://hf140dpkmh.execute-api.ap-south-1.amazonaws.com/wildriders' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
